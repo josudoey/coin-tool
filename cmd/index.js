f20cf7f3cea2721fae0793cdd532a670512959d4
Joey Fang <josudoey@gmail.com>
2017-08-10 10:43:11 +0800

    [why] patch tool

--- cmd/index.js
+++ cmd/index.js
@@ -2,4 +2,6 @@ const path = require('path')
 exports = module.exports = (prog) => {
   prog
     .option('--conf <config-path>', 'config file path default: <PWD>/etc/config.json', path.join(process.cwd(), 'etc', 'config.json'))
+    .option('--test', 'use testnet')
 }
+
--- cmd/tool.js
+++ cmd/tool.js
@@ -5,8 +5,12 @@ const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
 module.exports = function (prog) {
+  const getNetwork = function () {
+    const name = (prog.test) ? 'testnet' : 'bitcoin'
+    return bitcoin.networks[name]
+  }
   prog
-    .command('seed <mnemonic>')
+    .command('seed [mnemonic]')
     .description('use bip39 mnemonic code to seed code')
     .action(function (mnemonic, opts) {
       const seed = bip39.mnemonicToSeedHex(mnemonic).toString('hex')
@@ -17,7 +21,7 @@ module.exports = function (prog) {
     .command('seed2key <seed-hex>')
     .description('use bip32 generate hdnode key')
     .action(function (hex, opts) {
-      var node = bitcoin.HDNode.fromSeedHex(hex)
+      const node = bitcoin.HDNode.fromSeedHex(hex, getNetwork())
       console.log(node.toBase58())
     })
 
@@ -25,7 +29,7 @@ module.exports = function (prog) {
     .command('neutered <bip32-key>')
     .description('remove private key for bip32')
     .action(function (key, opts) {
-      var node = bitcoin.HDNode.fromBase58(key)
+      const node = bitcoin.HDNode.fromBase58(key, getNetwork())
       console.log(node.neutered().toBase58())
     })
 
@@ -33,7 +37,7 @@ module.exports = function (prog) {
     .command('wif <bip32-key>')
     .description('convert bip32 key to WIF key')
     .action(function (key, opts) {
-      var node = bitcoin.HDNode.fromBase58(key)
+      const node = bitcoin.HDNode.fromBase58(key, getNetwork())
       console.log(node.keyPair.toWIF())
     })
 
@@ -41,10 +45,46 @@ module.exports = function (prog) {
     .command('show <bip32-key>')
     .description('show key information')
     .action(function (key, opts) {
-      var node = bitcoin.HDNode.fromBase58(key)
+      const node = bitcoin.HDNode.fromBase58(key, getNetwork())
       console.log('chainCode', node.chainCode.toString('hex'))
       console.log('depth', node.depth)
       console.log('index', node.index)
+    })
+
+  prog
+    .command('derive <bip32-key> <path>')
+    .description('derive key')
+    .action(function (key, dPath, opts) {
+      const node = bitcoin.HDNode.fromBase58(key, getNetwork())
+      const newNode = node.derivePath(dPath)
+      console.log(newNode.toBase58())
+    })
+
+  prog
+    .command('p2pkh <bip32-key>')
+    .description('show p2pkh address')
+    .action(function (key, opts) {
+      const node = bitcoin.HDNode.fromBase58(key, getNetwork())
+      console.log(node.getAddress())
+    })
+
+  prog
+    .command('p2sh <m> <bip32-key...>')
+    .description('show p2sh address')
+    .action(function (mStr, keys, opts) {
+      const m = parseInt(mStr)
+      if (isNaN(m)) {
+        console.log('<m> require interger')
+        return
+      }
+      const pks = keys.map(function (key) {
+        const node = bitcoin.HDNode.fromBase58(key, getNetwork())
+        return node.getPublicKeyBuffer()
+      })
+      const redeemScript = bitcoin.script.multisig.output.encode(m, pks)
+      const scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
+      const address = bitcoin.address.fromOutputScript(scriptPubKey, getNetwork())
+      console.log(address)
     })
 }
 
